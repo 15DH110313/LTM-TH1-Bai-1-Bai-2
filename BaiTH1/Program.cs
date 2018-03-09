a55cf20837e792f9cb99e4ccb1ac242f2c5c6f9e
15DH110313 <reim.from.rnv@gmail.com>
2018-03-09 18:42:50 +0700

    BaiTH1

---
+++ BaiTH1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BaiTH1
+{
+    class Program
+    {
+         [STAThread]
+        static void Main(string[] args)
+        {
+            ////////////////BT1/////////////////
+            // Lấy Hostname
+            String strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name: " + strHostName);
+
+            // Tìm Hostname
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+
+            // Xuất danh sách IP
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +ipaddress.ToString());
+            }
+            ////////////////BT2/////////////////
+            // Nhập Hostname/IP cần phân giải
+            Console.Write("Hostname/IP: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.GetHostEntry(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            // Nếu là IP, tìm hostname
+            foreach (string s in e.Aliases) Console.WriteLine("Alias: {0} \n", s);
+
+            // Nếu là Hostname, phân giải tìm IP
+            foreach (IPAddress i in e.AddressList) Console.WriteLine("IP: {0}", i);
+        }
+    }
+}
